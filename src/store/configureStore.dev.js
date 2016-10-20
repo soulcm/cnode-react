69330d4334eda0373c3bba41f61e83ddb02eaec5
soulcm <liucm666@outlook.com>
2016-10-20 15:29:48 +0800

    去掉redux-logger

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,15 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
 import todoApp from 'reducers/index';
 
 import DevTools from 'containers/DevTools';
 
-const logger = createLogger();
 
 
 const enhancer = compose(
   //你要使用的中间件，放在前面
-  applyMiddleware(logger),
+  applyMiddleware(thunk),
   //必须的！启用带有monitors（监视显示）的DevTools
   DevTools.instrument()
 )
